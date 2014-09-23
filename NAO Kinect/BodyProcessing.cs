00cf8c0985fe739c9f0365c7c75b70daa8c90194
Austin Hughes <ahughes02@live.com>
2014-09-23 09:53:02 -0400

    Removed angleCalc and commented out currently unused variables

--- NAO Kinect/BodyProcessing.cs
+++ NAO Kinect/BodyProcessing.cs
@@ -64,8 +64,8 @@ namespace NAO_Kinect
                 var wristLeft = trackedBody.Joints[JointType.WristLeft].Position;
                 var wristRight = trackedBody.Joints[JointType.WristRight].Position;
 
-                var spineShoulder = trackedBody.Joints[JointType.SpineShoulder].Position;
-                var spineBase = trackedBody.Joints[JointType.SpineBase].Position;
+                //var spineShoulder = trackedBody.Joints[JointType.SpineShoulder].Position;
+                //var spineBase = trackedBody.Joints[JointType.SpineBase].Position;
 
                 var elbowLeft = trackedBody.Joints[JointType.ElbowLeft].Position;
                 var elbowRight = trackedBody.Joints[JointType.ElbowRight].Position;
@@ -125,52 +125,33 @@ namespace NAO_Kinect
             return bodyInfo;
         }
 
-        /// <summary>
-        /// Calculates angle between three points with P1 as vertex
-        /// </summary>
-        /// <param name="p1X"> X coordinate of point 1 </param>
-        /// <param name="p1Y"> Y coordinate of point 1 </param>
-        /// <param name="p2X"> X coordinate of point 2 </param>
-        /// <param name="p2Y"> Y coordinate of point 2 </param>
-        /// <param name="p3X"> X coordinate of point 3 </param>
-        /// <param name="p3Y"> Y coordinate of point 3 </param>
-        /// <returns> Angle calculated </returns>
-        private static float angleCalc(float p1X, float p1Y, float p2X, float p2Y, float p3X, float p3Y)
-        {
-            var p12 = (float)Math.Sqrt(((p1X - p2X) * (p1X - p2X)) + ((p1Y - p2Y) * (p1Y - p2Y)));
-            var p13 = (float)Math.Sqrt(((p1X - p3X) * (p1X - p3X)) + ((p1Y - p3Y) * (p1Y - p3Y)));
-            var p23 = (float)Math.Sqrt(((p2X - p3X) * (p2X - p3X)) + ((p2Y - p3Y) * (p2Y - p3Y)));
-
-            return (float)Math.Acos(((p12 * p12) + (p13 * p13) - (p23 * p23)) / (2 * p12 * p13));
-        }
-
         // Calculates angle between a<-b and b->c for situation a->b->c
         private static float angleCalc3D(CameraSpacePoint a, CameraSpacePoint b, CameraSpacePoint c)
         {
-            Vector3D ba = new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
-            Vector3D bc = new Vector3D(c.X - b.X, c.Y - b.Y, c.Z - b.Z);
+            var ba = new Vector3D(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+            var bc = new Vector3D(c.X - b.X, c.Y - b.Y, c.Z - b.Z);
 
-            float angle = (float)Vector3D.AngleBetween(ba, bc); // degrees
+            var angle = (float)Vector3D.AngleBetween(ba, bc); // degrees
             return (float)(Math.PI / 180) * angle; // radians
         }
 
         // Calculates angle between a<-b and b->c for situation a->b->c on XY plane only
         private static float angleCalcXY(CameraSpacePoint a, CameraSpacePoint b, CameraSpacePoint c)
         {
-            Vector3D ba = new Vector3D(a.X - b.X, a.Y - b.Y, 0);
-            Vector3D bc = new Vector3D(c.X - b.X, c.Y - b.Y, 0);
+            var ba = new Vector3D(a.X - b.X, a.Y - b.Y, 0);
+            var bc = new Vector3D(c.X - b.X, c.Y - b.Y, 0);
 
-            float angle = (float)Vector3D.AngleBetween(ba, bc); // degrees
+            var angle = (float)Vector3D.AngleBetween(ba, bc); // degrees
             return (float)(Math.PI / 180) * angle; // radians
         }
 
         // Calculates angle between a<-b and b->c for situation a->b->c on YZ plane only
         private static float angleCalcYZ(CameraSpacePoint a, CameraSpacePoint b, CameraSpacePoint c)
         {
-            Vector3D ba = new Vector3D(0, a.Y - b.Y, a.Z - b.Z);
-            Vector3D bc = new Vector3D(0, c.Y - b.Y, c.Z - b.Z);
+            var ba = new Vector3D(0, a.Y - b.Y, a.Z - b.Z);
+            var bc = new Vector3D(0, c.Y - b.Y, c.Z - b.Z);
 
-            float angle = (float)Vector3D.AngleBetween(ba, bc); // degrees
+            var angle = (float)Vector3D.AngleBetween(ba, bc); // degrees
             return (float)(Math.PI / 180) * angle; // radians
         }
     }
